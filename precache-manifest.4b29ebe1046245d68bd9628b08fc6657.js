6b3e16dbe4e43b3070e3277c1df66eb92128b3ec
Zubayer Ahmed Erik <30439531+ZubayerAhmed@users.noreply.github.com>
2021-03-11 19:26:13 +0600

    Updates

---
+++ precache-manifest.4b29ebe1046245d68bd9628b08fc6657.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f050c7df439ec20a86facbf19c7ec6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "84225883f1b7ed37dbe8",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/home/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84225883f1b7ed37dbe8",
+    "url": "/home/static/js/main.db690c2a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7a71e4db418116e40a7a7869715e260d",
+    "url": "/home/static/media/hashirshoaeb.7a71e4db.png"
+  },
+  {
+    "revision": "8a6546ab95bdee07add9d4e50b515ca4",
+    "url": "/home/static/media/resume.8a6546ab.pdf"
+  },
+  {
+    "revision": "7a71e4db418116e40a7a7869715e260d",
+    "url": "/home/static/media/zubayer.7a71e4db.png"
+  }
+]);
