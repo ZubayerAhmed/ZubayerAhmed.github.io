9b646d427edd5cb7c1d3a3c5ec5eb840ba1df6ef
Zubayer Ahmed Erik <30439531+ZubayerAhmed@users.noreply.github.com>
2021-03-11 20:21:58 +0600

    Updates

---
+++ precache-manifest.a335d66d66382ba3e688c22d3dd835a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13577e27c89563e41ac26658bf9987f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5399b759bca64d96d4f2",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5399b759bca64d96d4f2",
+    "url": "/static/js/main.b259685a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8a6546ab95bdee07add9d4e50b515ca4",
+    "url": "/static/media/resume.8a6546ab.pdf"
+  },
+  {
+    "revision": "7a71e4db418116e40a7a7869715e260d",
+    "url": "/static/media/zubayer.7a71e4db.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.68ef586538b166892da26e8b80f69605.js"
+  "/precache-manifest.a335d66d66382ba3e688c22d3dd835a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
